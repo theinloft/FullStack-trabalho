9a29d50ec8f6477e1472f627485e008a757c9e63
Thiago R Einloft <theinloft@gmail.com>
2026-04-26 01:45:10 -0300

    feature: mapeando a API de pedido dentro de App.ts e data-source.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { produtoRotas } from "./router/produto-router";
+
 import { ProdutoController } from "./controller/produto-controller";
 import { ProdutoService } from "./service/produto-service";
 import { ProdutoRepositoryMem } from "./repository/produto-repository-mem";
@@ -8,6 +9,10 @@ import { Produto } from "./entity/produto";
 import "reflect-metadata";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger";
+import { pedidoRotas } from './router/pedido-router';
+import { PedidoService } from "./service/pedido-service";
+import { PedidoController } from "./controller/pedido-controller";
+import { Pedido } from "./entity/pedido";
 
 const app = express();
 const port = 3000;
@@ -21,6 +26,9 @@ AppDataSource.initialize().then((async) => {
   const produtoRepository = AppDataSource.getRepository(Produto);
   const produtoService = new ProdutoService(produtoRepository);
   const produtoController = new ProdutoController(produtoService);
+  const pedidoRepository = AppDataSource.getRepository(Pedido);
+  const pedidoService = new PedidoService(pedidoRepository);
+  const pedidoController = new PedidoController(pedidoService);
 
  app.use(
     "/api-docs",
@@ -33,6 +41,8 @@ AppDataSource.initialize().then((async) => {
   );
   
   app.use("/api/produto", produtoRotas(produtoController));
+  app.use('/api/pedido', pedidoRotas(pedidoController));
+
 
   app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}, para acessar a documentação da API: http://localhost:${port}/api-docs`);
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,17 @@
 import { DataSource } from "typeorm";
 import { Produto } from "./entity/produto";
 import { Categoria } from "./entity/categoria";
+import { Cliente } from "./entity/cliente";
+import { Pedido } from "./entity/pedido";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
     username: "postgres",
-    password: "senacrs",
+    password: "123456",
     database: "crud-produtos",
-    entities: [Produto, Categoria],
+    entities: [Produto, Categoria,Cliente,Pedido],
     logging: true,
     synchronize: true,
 })
