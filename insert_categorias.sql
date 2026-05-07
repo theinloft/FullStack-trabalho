65d585e36d159592b9283336bc3ccacca6204cf0
theinloft <theinloft@gmail.com>
2026-05-07 18:05:14 -0300

    Feature: adicionando insert de categorias
    Feature: adicionando insert de categorias

---
+++ insert_categorias.sql
@@ -0,0 +1,4 @@
+
+insert into categoria(id,categoria) values(1,'Camisetas e Camisas');
+insert into categoria(id,categoria) values(2,'Calças e Bermudas');
+insert into categoria(id,categoria) values(3,'Acessórios');
