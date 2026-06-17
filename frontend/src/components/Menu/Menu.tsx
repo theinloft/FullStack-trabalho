81ff65d615f9df0217d4e0f6cc4303ff4f66baa9
THIAGO ROCHA EINLOFT <02546733095@senacrs.edu.br>
2026-06-16 21:55:12 -0300

    bugfix: ajustando o menu e o carroussel para ficar responsivo.

--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -5,16 +5,25 @@ function Menu() {
     <>
       <nav className="menu">
         <div className="logo">SHRED_LAB</div>
-        <ul className="nav-links">
-          <li>SKATE</li>
-          <li>SURF</li>
-          <li>COLEÇÕES</li>
-        </ul>
-        <span className="material-icons">search</span>
-        <span className="material-symbols-outlined">shopping_cart</span>
-        <span className="material-icons">account_circle</span>
+        <div className="lista">
+          <ul className="nav-links">
+            <li>SKATE</li>
+            <li>SURF</li>
+            <li>COLEÇÕES</li>
+          </ul>
+
+        </div>
+
+        <div className="lista-responsive">
+        <a className="material-icons">dehaze </a>
+      </div>
+        <a className="material-icons">search</a>
+        <a className="material-symbols-outlined">shopping_cart</a>
+        <a className="material-icons">account_circle</a>
       </nav>
       <hr className="linha-divisor" />
+      
+
     </>
   );
 }
