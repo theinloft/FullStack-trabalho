c152e4f62bd31386d87f41e3f04e27ee8ed7d92f
Thiago R Einloft <theinloft@gmail.com>
2026-06-17 00:28:43 -0300

    Feature: adicionando o componente Menu no page.tsx, para exibir o componente na página principal.

--- frontend-next/src/app/page.tsx
+++ frontend-next/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Carroussel from '@/components/Carroussel/Carroussel'
+import Carroussel from "@/components/Carroussel/Carroussel";
+import Menu from "@/components/Menu/Menu";
 
 export default function Home() {
   return (
     <main>
+      <Menu />
       <Carroussel />
     </main>
-  )
+  );
 }
