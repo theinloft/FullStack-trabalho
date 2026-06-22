508947ceb7761f77eaddb3243c3ab7f9d10ba8e8
Thiago R Einloft <theinloft@gmail.com>
2026-06-22 15:07:01 -0300

    Feature: adicionando o Layout.tsx, pois e nele que renderiza os componentes que irão se repetir nas demais paginas, por exemplo, em login e cadastro, para aparecer o menu acima da pagina.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,18 @@ import Login from "./Pages/Login/Login";
 import Home from "./Pages/Home";
 import CadastroCliente from "./Pages/CadastroCliente/Cadastro-Cliente";
 import Painel from "./Pages/Painel/Painel";
+import Layout from "./Pages/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Login" element={<Login />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/cadastro-cliente" element={<CadastroCliente />} />
-        <Route path="/painel" element={<Painel />} />
+        <Route element={<Layout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/cadastro-cliente" element={<CadastroCliente />} />
+          <Route path="/painel" element={<Painel />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,10 +1,8 @@
-import Menu from "../components/Menu/Menu";
 import Carroussel from "../components/Carroussel/Carroussel";
 
 function Home() {
   return (
     <>
-      <Menu />
       <Carroussel />
     </>
   );
