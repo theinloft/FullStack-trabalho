637739efc9f390fe7cb3d6d70f6b962e2449460d
Eduardo Berno <Eduardo.berno@gmail.com>
2026-06-29 02:39:03 -0300

    Feature: incorporado as imagens redimensionadas para cada tipo de dispositivo

--- frontend/src/components/about/about.tsx
+++ frontend/src/components/about/about.tsx
@@ -2,6 +2,9 @@ import './about.css';
 import image1 from './img/image1.svg'
 import image1comprimida from './img/image1comprimida.svg'
 import image1pequena from './img/image1pequena.svg'
+import contacts1 from './img/Contacts1.svg'
+import contacts2 from './img/Contacts2.svg'
+import contacts3 from './img/Contacts3.svg'
 
 export function About() {
     return (
@@ -20,16 +23,14 @@ export function About() {
             </div>
             <div className="image left">
                 <picture>
-                    <source media="(min-width: 768px)" srcSet="https://placehold.co/600x400" />
-                    <source media="(min-width: 465px)" srcSet="https://placehold.co/400" />
-                    <img src="https://placehold.co/300" alt="imagem placeholder 300x300"></img>
+                    <source media="(min-width: 768px)" srcSet={contacts1} />
+                    <source media="(min-width: 465px)" srcSet={contacts2} />
+                    <img src={contacts3}alt="imagem placeholder 300x300"></img>
                 </picture>
             </div>
             <div className="item">
-                <h3>Título</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquam voluptatibus ipsam minima commodi
-                    cum aspernatur molestiae error, provident cupiditate voluptatum quis aperiam. Laboriosam, adipisci sed
-                    suscipit ipsa rem animi.</p>
+                <h3>O CRM de vendas feito sob medida para seu negócio</h3>
+                <p>Migre facilmente para uma ferramenta adequada para cada etapa da jornada dos seus clientes. O Shred_lab é muito mais que um funil de vendas.</p>
                 <a href="" className="btn-know-more">Saiba mais</a>
             </div>
         </section>
