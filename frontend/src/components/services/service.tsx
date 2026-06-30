641f85bda428e33206ce6d83a68a2aa6cdd9edec
Eduardo Berno <eduardo.berno@gmail.com>
2026-06-30 20:33:02 -0300

    Bugfix: ajustado imagens da seção service e  ajustado a fonte da seção about

--- frontend/src/components/services/service.tsx
+++ frontend/src/components/services/service.tsx
@@ -1,4 +1,8 @@
 import './service.css';
+import image1 from './img/produtos2.png';
+import image2 from './img/clientes2.png';
+import image3 from './img/projeto3.jpeg';
+import image4 from './img/projeto4.jpg';
 
 
 export function Service() {
@@ -6,21 +10,25 @@ export function Service() {
         <section className="projects" id="projects">
             <div className="project imagem1">
                 <a href="">
+                    <img src={image1} alt="imagem de produtos"></img>
                     <p>produtos</p>
                 </a>
             </div>
             <div className="project imagem2">
                 <a href="">
+                    <img src={image2} alt="imagem de produtos"></img>
                     <p>clientes</p>
                 </a>
             </div>
             <div className="project imagem3">
                 <a href="">
+                    <img src={image3} alt="imagem de produtos"></img>
                     <p>Pedidos</p>
                 </a>
             </div>
             <div className="project imagem4">
                 <a href="">
+                    <img src={image4} alt="imagem de produtos"></img>
                     <p>painel</p>
                 </a>
             </div>
